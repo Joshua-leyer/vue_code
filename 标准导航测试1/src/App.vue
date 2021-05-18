75b0f3d1610bc221ebd8c5fa04352db67c1fd49c
Patrick star and Sponge bob <40789418+Joshua-leyer@users.noreply.github.com>
2021-05-18 10:07:33 +0800

    down 7

---
+++ 标准导航测试1/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1']">
+
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">
+                <router-link to="/stm32">STM32</router-link>
+              </el-menu-item>
+              <el-menu-item index="1-2">
+                <router-link to="/about">about</router-link>
+              </el-menu-item>
+              <el-menu-item index="1-3">
+                <router-link to="/list">list</router-link>
+              </el-menu-item>
+            </el-menu-item-group>
+
+          </el-submenu>
+
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>Joshua</span>
+        </el-header>
+        
+        <el-main>
+              <router-view> 
+
+              </router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+  name: 'App',
+  data() {
+    return {
+      activeNames: ["1"]
+    }
+  },
+  methods: {
+    handleChange(e) {
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-header {
+    background-color: #93dfff;
+    color: #333;
+    line-height: 60px;
+    font-size: 1.5rem !important;
+  }
+  
+
+
+  .el-aside {
+    background-color: #93dfff !important;
+
+    color: #333;
+  }
+
+
+</style>
---
+++ 标准导航测试1/src/components/ListHover.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="List-content"   >
+        <div v-for="item in items" :key="item.id" @mouseup="up($event, item)" @mousedown="down($event, item)" class="item-body" @mouseenter="enter($event, item)" >
+
+                <span class="switch-title">#{{ item.id }}  this is my bulabula {{ item.name }}</span>
+                    <el-switch
+                        v-model="item.switch"
+                        active-color="#13ce66"
+                        inactive-color="#ff4949">
+                    </el-switch>
+        </div>
+    </div>
+    
+</template>
+
+
+
+<script>
+export default {
+    name: 'List',
+    data() {
+        return {
+            ChangeStatus: false, //这个值是true时候 会改变对应的Swtich值
+            items: [
+                {id: 1, name: "派大星", switch: true},
+                {id: 2, name: "派大星", switch: true},
+                {id: 3, name: "派大星", switch: true},
+                {id: 4, name: "派大星", switch: true}
+            ],
+            value: false
+        }
+    },
+    methods: {
+        down(e, item) {
+            // console.log('down')
+            this.ChangeStatus = !this.ChangeStatus
+            //点击就出发change
+            item.switch = !item.switch
+        },
+        enter(e, item){
+            let ele = e.target
+            // console.log('enter')
+            if (this.ChangeStatus == true) {
+                item.switch = !item.switch
+            }
+        },
+        up(e, item) {
+            this.ChangeStatus = false
+        }
+    },
+    mounted() {
+        
+    }
+
+};
+</script>
+
+<style >
+    .switch-title {
+        -webkit-user-select:none;
+
+        -moz-user-select:none;
+
+        -ms-user-select:none;
+
+        user-select:none;
+    }
+
+    .item-body {
+        width: 60%;
+        height: 46px;
+        margin: 2px;
+        border: 1px solid black;
+        border-radius: 5px;
+        padding: 3px;
+    }
+
+    .item-body {
+        margin: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .item-body:hover {
+        background: rgb(175, 240, 245);
+
+    }
+    .switch-title {
+        flex: 5;
+    }
+    .el-switch {
+        flex: 1;
+    }
+
+
+
+</style>
